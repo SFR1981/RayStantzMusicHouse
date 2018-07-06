ce23afa2c3c31a688a5cc337741c01bdc6b8a688
Stephen Rooney <s.f.rooney@gmail.com>
2018-07-06 13:36:12 +0100

    xylophone tested

--- src/main/java/Xylophone.java
+++ src/main/java/Xylophone.java
@@ -1,2 +1,19 @@
-public class Xylophone {
+public class Xylophone extends Instrument{
+    int notes;
+    String material;
+    public Xylophone(String name, String type, int notes, String material, String manufacturer, String colour, String sound, double boughtPrice, double sellingPrice) {
+        super(name, type, manufacturer, colour, sound, boughtPrice, sellingPrice);
+        this.notes = notes;
+        this.material = material;
+        }
+
+
+
+        public int getNotes(){
+        return this.notes;
+        }
+
+        public String getMaterial(){
+        return this.material;
+        }
 }
---
+++ src/test/java/XylophoneTest.java
@@ -0,0 +1,81 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class XylophoneTest {
+
+    Xylophone xylophone;
+
+    @Before
+    public void before() {
+        xylophone = new Xylophone("hammers","xylophone", 16, "wood", "Xylem", "mahogany", "ding dong", 70.00, 145.99);
+    }
+
+    @Test
+    public void getNotes() {
+        assertEquals(16, xylophone.getNotes());
+    }
+
+    @Test
+    public void getMaterial() {
+        assertEquals("wood", xylophone.getMaterial());
+    }
+
+
+    @Test
+    public void play() {
+        assertEquals("playing hammers (xylophone) it makes a ding dong sound", xylophone.play());
+    }
+
+    @Test
+    public void calculateMarkUpPrice() {
+        assertEquals(75.99, xylophone.calculateMarkUpPrice(), 0.01);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("hammers", xylophone.getName());
+
+    }
+
+
+
+
+    @Test
+    public void canGetType(){
+        assertEquals("xylophone", xylophone.getType());
+
+    }
+
+    @Test
+    public void canGetManufacturer(){
+        assertEquals("Xylem", xylophone.getManufacturer());
+
+    }
+
+    @Test
+    public void canGetColour(){
+        assertEquals("mahogany", xylophone.getColour());
+
+    }
+
+    @Test
+    public void canGetSound(){
+        assertEquals("ding dong", xylophone.getSound());
+
+    }
+
+
+    @Test
+    public void canGetBoughtPrice(){
+        assertEquals(70.00, xylophone.getBoughtPrice(), 0);
+
+    }
+
+    @Test
+    public void canGetSellingPrice(){
+        assertEquals(145.99, xylophone.getSellingPrice(), 0);
+
+    }
+}
