4a1921b44c3bcf4855262a44769af2bdaddc8581
Stephen Rooney <s.f.rooney@gmail.com>
2018-07-06 11:38:09 +0100

    initial commit

---
+++ src/main/java/Instrument.java
@@ -0,0 +1,23 @@
+public abstract class Instrument {
+
+    private String name;
+    private String type;
+    private String manufacturer;
+    private String colour;
+    private String sound;
+    private double boughtPrice;
+    private double sellingPrice;
+
+    public Instrument(String name,  String type, String manufacturer, String colour, String sound, double boughtPrice, double sellingPrice){
+        this.name = name;
+        this.type = type;
+        this.manufacturer = manufacturer;
+        this.colour = colour;
+        this.sound = sound;
+        this.boughtPrice =boughtPrice;
+        this.sellingPrice = sellingPrice;
+    }
+
+
+    public
+}
